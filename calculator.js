a2b127b54ad4922d2d8013d21eea0322f5a1479f
muhammed-jamiu <anatemuhammed@gmail.com>
2024-11-03 14:32:06 +0100

    second commit

--- calculator.js
+++ calculator.js
@@ -1,1 +1,22 @@
-alert("new -Calculator");
+// CALCULATOR PROGRAM
+
+const display = document.querySelector("#display");
+const appendToDisplay = (input) => {
+    display.value += input;
+};
+
+const clearDisplay = () => {
+    display.value = " ";
+};
+
+function calculate() {
+    try {
+        display.value=eval(display.value)
+    } catch (error) {
+        display.value = "Syntax error";
+    }
+}
+
+const deleteDisplay = () => {
+    console.log(display.value.length-1)
+}
